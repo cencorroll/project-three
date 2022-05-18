91bd92841e0d48877ecbe2887df45b18b5ced8cb
mattccs <mattcc.shek@gmail.com>
2022-05-18 14:34:28 +0100

    linked frontend

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,6 @@ import User from '../models/users.js'
 export const getProfile = async (req, res) => {
   try {
     const profile = await User.findById(req.verifiedUser._id).populate('createdCities')
-    return res.status(200).json(profile)
   } catch (error) {
     console.log(error)
     return res.status(401).json({ message: 'Unauthorised' })
