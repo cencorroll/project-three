adfc322c37815cfa32334bd5d070cace61a3b6bb
Nicholas <razvan.botorog@yahoo.com>
2022-05-20 16:25:31 +0300

    day5

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import PageNotFound from './components/utilities/PageNotFound'
 
 
 import CitiesIndex from './components/Cities/CitiesIndex'
+import GetOneHotel from './components/Hotels/GetOneHotel'
+import GetFun from './components/Fun/OneThingsToDo'
 // import FullPageCities from '../src/components/Cities/FullPageCities'
 const App = () => {
 
@@ -34,6 +36,8 @@ const App = () => {
           {/* <Route path="/api/cities/:id/fun" element={<AllThingsToDo />} /> */}
           <Route path="/api/cities/:id/fun/:funId" element={<OneThingsToDo />} />
           <Route path='/api/cities/:id/restaurants/:restaurantId' element={<GetOneRestaurant />} />
+          <Route path='/api/cities/:id/hotels/:hotelId' element={<GetOneHotel />} />
+          <Route path='/api/cities/:id/fun/:funId' element={<GetFun />} />
           <Route path="*" element={<PageNotFound />} />
 
 
