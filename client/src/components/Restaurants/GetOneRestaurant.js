04eb4ac389737e61844e711368db8395505beda7
Nicholas <razvan.botorog@yahoo.com>
2022-05-20 13:27:02 +0300

    day 5

--- client/src/components/Restaurants/GetOneRestaurant.js
+++ client/src/components/Restaurants/GetOneRestaurant.js
@@ -2,7 +2,16 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate, Link, useParams } from 'react-router-dom'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
+
+
 const GetOneRestaurant = () => { 
+
+  const navigate = 
 
   return (
     <h1>Restaurant</h1>
