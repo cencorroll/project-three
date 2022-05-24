7a737df0dee85ea56a388978beecdc37874b8b36
Nicholas <razvan.botorog@yahoo.com>
2022-05-24 12:40:26 +0300

    reviews

--- client/src/components/Reviews/StarRating.js
+++ client/src/components/Reviews/StarRating.js
@@ -1,29 +1,39 @@
 import React, { useState } from 'react'
 import { FaStar } from 'react-icons/fa'
 
-const StarRating = () => { 
+const StarRating = ( { rating } ) => { 
   
-  const [ rating, setRating ] = useState(null)
+  const [ ratings, setRatings ] = useState(null)
   const [ hover, setHover ] = useState(null)
   
+  const stars = [ FaStar, FaStar, FaStar, FaStar, FaStar ].map((star, index) => {
+    if (index < Number(rating)) {
+      return <span key={index}>{star}</span>
+    } else {
+      return <span key={index} className='grey'>{star}</span>
+    }
+  })
+
   return (
-    <div>
-      {[ ...Array(5)].map((star, i)=>{
-        const ratingValue = i + 1
-        return (
-          <label key={i}>
-            <input type='radio' name='rating' value={ratingValue} 
-              onClick={() => setRating(ratingValue)} 
-              // onChange={handleChange}
-            />
-            <FaStar className='star' color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9' } 
-              onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => setHover(null)}
-              size={ 40 }
-            />
-          </label>
-        )
-      })}
-    </div>
+    <div className='stars'>{stars}</div>
   )
 }
+// return (
+//   <div>
+//     {[ ...Array(5)].map((star, i)=>{
+//       const ratingValue = i + 1
+//       return (
+//         <label key={i}>
+//           <input type='radio' name='rating' value={ratingValue} 
+//             onClick={() => setRatings(ratingValue)} 
+//           />
+//           <FaStar className='star' color={ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9' } 
+//             onMouseEnter={() => setHover(ratingValue)}
+//             onMouseLeave={() => setHover(null)}
+//             size={ 40 }
+//           />
+//         </label>
+//       )
+//     })}
+//   </div>
+// )
