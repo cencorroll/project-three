a4ce381ea9940adafe98d93cec1c291157748d41
mattccs <mattcc.shek@gmail.com>
2022-05-25 11:28:08 +0100

    fix

--- controllers/subSchema.js
+++ controllers/subSchema.js
@@ -139,7 +139,7 @@ export const getSingleRestaurant = async (req, res) => {
 export const addRestaurantReview = async (req, res) => { 
   const { id, restaurantId } = req.params
   try {
-    const city = await (await City.findById(id).populate('restaurants').populate('reviews').populate('restaurants.reviews.owners'))
+    const city = await (await City.findById(id).populate('restaurants').populate('reviews'))
     if (!city) return res.status(404).json({ message: 'City Not Found' })
     const restaurant = await city.restaurants.find(restaurant => restaurant._id.toString() === restaurantId)
     if (!restaurant) {
